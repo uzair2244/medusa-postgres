2a3ed1423c798d6e72f0101f528f668359cbf043
uzair2244 <uzirao17@gmail.com>
2024-04-21 02:27:30 -0500

    --m

--- medusa-config.js
+++ medusa-config.js
@@ -47,9 +47,9 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
-      // develop: {
-      //   open: process.env.OPEN_BROWSER !== "false",
-      // },
+      develop: {
+        open: process.env.OPEN_BROWSER !== "false",
+      },
     },
   },
 ];
